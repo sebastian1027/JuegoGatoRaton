01402f9dcde25dd0c9c2038c2996ca041a7a4e94
sebastian López Fernández <sebastian Lpez Fernndez>
2017-09-25 10:13:58 -0500

    Programa con comentarios

--- JuegoGatoRaton.c
+++ JuegoGatoRaton.c
@@ -13,49 +13,50 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
-int STATIC = 4;
-int DINAMIC = 1;
+
+int STATIC = 4; //luz encendida que representa al raton
+int DINAMIC = 1;  //Luz encendidad durante un segundo que representa al gato
 
 void interrupt isr(void){
-        INTCONbits.INTF = 0;
-        if(PORTBbits.RB7&&DINAMIC!=128)
-            DINAMIC = DINAMIC*2;
-        if(PORTBbits.RB7==0&&DINAMIC!=1)
-                DINAMIC = DINAMIC/2;
+        INTCONbits.INTF = 0; //interrupcion
+        if(PORTBbits.RB7&&DINAMIC!=128) //verifica que no haya desborade, es decir si llega al led MSB no pase y no genere error
+            DINAMIC = DINAMIC*2;    //para avanzar hacia la derecha
+        if(PORTBbits.RB7==0&&DINAMIC!=1) //verifica que no haya desborade, es decir si llega al led LSB no pase y no genere error
+                DINAMIC = DINAMIC/2;    //para la izquierda
     }
-void validar(){
-    if(STATIC==DINAMIC){
-            PORTC = STATIC;
-            PORTDbits.RD0 = 1;
-            __delay_ms(3000);
-            PORTDbits.RD0 = 0;
+void validar(){ //funcion validar
+    if(STATIC==DINAMIC){ //verifica que los 2 leds esten en la misma posicion
+            PORTC = STATIC; //el puerto c toma el valor de "DINAMIC"
+            PORTDbits.RD0 = 1;  //en el momento que los 2 leds esten en la misma posicion enciende otro led que esta en el pin 0 del puerto D
+            __delay_ms(3000);   //enciende el led por 3 segundos
+            PORTDbits.RD0 = 0;  //pasados los 3 segundos apague el led
         }
         else
-            PORTC = STATIC + DINAMIC;
+            PORTC = STATIC + DINAMIC;   //de no atraparlo siga con el juego
 }
 void main(void) {
-    TRISC = 0;
-    PORTC = 1;
-    TRISB = 0xFF;
-    ANSEL = 0;
+    TRISC = 0;  //puerto c como salidas
+    PORTC = 1;  
+    TRISB = 0xFF;   //puerto B como entrada
+    ANSEL = 0;      //desactiva entradas analogicas
     ANSELH = 0;
-    TRISD = 0;
-    PORTD = 0;    
+    TRISD = 0;      //puerto D como salidas
+    //PORTD = 0;    
     //INTCON = 10001000; //interrupciones de RB4 y RB7
     INTCONbits.GIE = 1;
-    INTCONbits.INTE = 1;
+    INTCONbits.INTE = 1;        //banderas de interrupcion
     //INTCONbits.RBIE = 1;
     //INTCONbits.RBIF = 0;
     INTCONbits.INTF = 0;
-    int aux;
-    int aux2=0;
+    int aux;    //variables auxiliar
+    int aux2=0; //variables auxiliar2
     while (1){
         do
-        {STATIC = rand()%7; 
+        {STATIC = rand()%7; //ejecuta la funcion random
         STATIC = pow(2, STATIC); //PORTC como 2 a la potencia(random)
         }while(STATIC == DINAMIC);
         aux = 0;
-        while(aux<5){
+        while(aux<5){   // hace que el led que representa al raton este con una luz intermitente
             validar(); 
             __delay_ms(50);
             aux2 = STATIC;
